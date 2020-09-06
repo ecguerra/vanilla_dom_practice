21123ab139fcc1b82f9ef26c3b19aa17861fb407
Elizabeth Guerra <ecguerra@outlook.com>
2020-09-05 21:35:21 -0400

    DOM console.logs complete

---
+++ dom_practice.js
@@ -0,0 +1,10 @@
+const minuteman = document.querySelector('#minuteman')
+console.log(minuteman)
+
+const welcome = document.querySelector('h2')
+console.log(welcome)
+
+welcome.innerText = 'Welcome to Lexington!!!'
+
+const content = document.querySelectorAll('.content')
+console.log(content)
