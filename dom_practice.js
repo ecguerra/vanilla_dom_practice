636058bfc3e413d72e1f494bb7b6b78c9fe8dcbb
Elizabeth Guerra <ecguerra@outlook.com>
2020-09-05 21:50:22 -0400

    main deliverable complete, updated DOM tree

--- dom_practice.js
+++ dom_practice.js
@@ -8,3 +8,12 @@ welcome.innerText = 'Welcome to Lexington!!!'
 
 const content = document.querySelectorAll('.content')
 console.log(content)
+
+const list = document.querySelectorAll('li')
+list[4].style.color = 'mediumvioletred'
+list[1].innerText = 'Jamboree sandwich at the Jambo-ree'
+
+const myOldHouse = document.createElement('img')
+myOldHouse.setAttribute('src','./myoldhouse.jpg')
+
+content[1].appendChild(myOldHouse)
